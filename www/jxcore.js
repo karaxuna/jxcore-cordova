b302dcc09d1d0cfa80f69121d4d780f9abd28f8c
karaxuna <kaxabernet@gmail.com>
2016-01-11 06:36:13 +0400

    jxcore cordova api changed

--- www/jxcore.js
+++ www/jxcore.js
@@ -8,17 +8,17 @@ var channel = require('cordova/channel'),
 channel.createSticky('onJXcoreReady');
 channel.waitForInitialization('onJXcoreReady');
 var jxcore_device = {
-  ios: (navigator.userAgent.match(/iPad/i)) == "iPad" || (navigator.userAgent.match(/iPhone/i)) == "iPhone",
-  android: (navigator.userAgent.match(/Android/i)) == "Android"
+  ios: (navigator.userAgent.match(/iPad/i)) == 'iPad' || (navigator.userAgent.match(/iPhone/i)) == 'iPhone',
+  android: (navigator.userAgent.match(/Android/i)) == 'Android'
 };
 
 if (!jxcore_device.ios && !jxcore_device.android) {
   var counter = 0, errmsg = 'JXcore plugin: Device type is unkown. Defaulting to Android';
   var inter = setInterval(function () {
-    if (typeof log != "undefined") {
+    if (typeof log != 'undefined') {
       log(errmsg, 'red');
     } else if (++counter > 400) {
-      if (typeof console != "undefined") {
+      if (typeof console != 'undefined') {
         console.log(errmsg);
       }
     } else {
@@ -41,7 +41,7 @@ function callNative(name, args, callback) {
             try {
               data = JSON.parse(data);
             } catch (e) {
-              log("Error:", e.message);
+              log('Error:', e.message);
               return;
             }
             if (!Array.isArray(data)) {
@@ -63,59 +63,45 @@ function callNative(name, args, callback) {
 }
 
 channel.onCordovaReady.subscribe(function () {
-  callNative("onResume", null, function () {
+  callNative('onResume', null, function () {
     channel.onJXcoreReady.fire();
   });
 });
 
-function jxcore(x) {
-  if (!(this instanceof jxcore)) {
-    return new jxcore(x);
-  }
-  this.name = x;
-}
-
 var initialized = false;
-jxcore.isReady = function (callback) {
+(function checkReady() {
   if (initialized) return;
-  callNative("isReady", [], function (ret) {
-    if (ret) {
+  callNative('isReady', [], function (ret, err) {
+    if (err) {
+      alert(err);
+    } else if (ret) {
       initialized = true;
-      callback();
+      document.onjxready();
     } else {
       setTimeout(function () {
-        jxcore.isReady(callback);
+        checkReady();
       }, 5);
     }
   });
-};
+})();
 
-var isFunction = function (__$) {
-  var _$ = {};
-  return __$ && _$.toString.call(__$) === '[object Function]';
-};
+var jxcore = module.exports = {};
 
-var callFunction = function (name, params, callback) {
-  var args = {};
-  args.methodName = name;
-  args.params = params;
-
-  callNative("Evaluate", ["Mobile.executeJSON(" + JSON.stringify(args)], callback);
-};
-
-jxcore.prototype.call = function () {
+jxcore.Call =
+jxcore.call = function (methodName) {
   var ln = arguments.length;
-  var methodName = this.name;
   var callback = null;
-  var args = [];
+  var args;
 
-  if (ln) {
-    ln -= isFunction(arguments[ln - 1]) ? 1 : 0;
-    for (var i = 0; i < ln; i++) {
-      args[i] = arguments[i];
-    }
-    if (ln != arguments.length)
-      callback = arguments[ln];
+  if (!methodName || typeof methodName !== 'string') {
+    throw 'Method name must be string';
+  }
+
+  if (isFunction(arguments[ln - 1])) {
+    callback = arguments[ln - 1];
+    args = Array.prototype.slice.call(arguments, 1, ln - 1);
+  } else {
+    args = Array.prototype.slice.call(arguments, 1, ln);
   }
 
   callFunction(methodName, args, callback);
@@ -123,17 +109,39 @@ jxcore.prototype.call = function () {
 };
 
 var localMethods = {};
-jxcore.prototype.register = function (callback) {
+jxcore.register = function (methodName, callback) {
   if (!isFunction(callback)) {
-    throw new TypeError("callback needs to be a function");
+    throw new TypeError('Callback needs to be a function');
   }
 
-  localMethods[this.name] = callback;
+  localMethods[methodName] = callback;
   return this;
 };
 
+jxcore.Start =
+jxcore.start = function () {
+  var len = arguments.length;
+  var fileName;
+  var callback;
 
-var callLocalMethods = function () {
+  if (typeof arguments[0] === 'string') {
+    fileName = arguments[0];
+    if (isFunction(arguments[1])) {
+      callback = arguments[1];
+    }
+  } else {
+    fileName = 'app.js';
+    if (isFunction(arguments[0])) {
+      callback = arguments[0];
+    }
+  }
+
+  callFunction('registerUIMethod', ['callLocalMethods'], callLocalMethods);
+  callFunction('loadMainFile', [fileName], callback);
+  return this;
+};
+
+function callLocalMethods() {
   if (!localMethods.hasOwnProperty(arguments[0]))
     return;
 
@@ -144,7 +152,7 @@ var callLocalMethods = function () {
     args = Array.prototype.slice.call(arguments, 1);
   } else {
     args = arguments[1];
-    if (args.length && args[args.length - 1].hasOwnProperty("JXCORE_RETURN_CALLBACK")) {
+    if (args.length && args[args.length - 1].hasOwnProperty('JXCORE_RETURN_CALLBACK')) {
       call_id = args[args.length - 1].JXCORE_RETURN_CALLBACK;
       args[args.length - 1] = function () {
         var target = jxcore(call_id);
@@ -152,25 +160,31 @@ var callLocalMethods = function () {
       };
     }
   }
+  
   localMethods[arguments[0]].apply(null, args);
 };
 
-jxcore.prototype.loadMainFile = function (callback) {
-  callFunction("registerUIMethod", ["callLocalMethods"], callLocalMethods);
-  callFunction("loadMainFile", [this.name], callback);
-  return this;
+function isFunction(__$) {
+  var _$ = {};
+  return __$ && _$.toString.call(__$) === '[object Function]';
 };
 
-module.exports = jxcore;
+function callFunction(name, params, callback) {
+  var args = {};
+  args.methodName = name;
+  args.params = params;
+
+  callNative('Evaluate', ['Mobile.executeJSON(' + JSON.stringify(args)], callback);
+};
 
 function onPause() {
-  callNative("onPause");
+  callNative('onPause');
 }
 
-document.addEventListener("pause", onPause, false);
+document.addEventListener('pause', onPause, false);
 
 function onResume() {
-  callNative("onResume");
+  callNative('onResume');
 }
 
-document.addEventListener("resume", onResume, false);
+document.addEventListener('resume', onResume, false);
